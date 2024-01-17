3f5253702203af273632c75d17d802cb7adbd644
MeshackKorir <meshackkorir100@gmail.com>
2024-01-17 15:25:40 +0300

    fotter

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-let todoForm = document.querySelector('#todoform')
+let todosContainer = document.querySelector('.todos')
+
+let todos = []
+
+let todoform = document.querySelector('#todoform')
 let newtodo = document.getElementById("createtodo")
 let checkedstatus = document.querySelector(".checkbox")
 
-todoForm.addEventListener("submit", (event) => {
-    event.preventDefault()
+todoform.addEventListener("submit", (e)=>{
+    e.preventDefault()
 
-    +(newtodo.Value.trim() !=="");{
+    if(newtodo.value.trim() !== ""){
         let todo = {
-            taskname: newtodo.Value.trim(),
+            taskname: newtodo.value.trim(),
             status: checkedstatus.checked
         }
 
         todos.push(todo)
- }
+
+        console.log(todos);
+
+        localStorage.setItem('todos', JSON.stringify(todos))
+
+        displayTodos()
+    }
 })
+
+// Display todos
+let displayTodos = function(){
+
+    let taskItems = localStorage.getItem("todos")
+
+    taskItems = JSON.parse(taskItems)
+
+    let tasks = document.querySelectorAll('.todos .todo')
+
+    tasks.forEach(el=>{
+        el.remove()
+    })
+
+
+    taskItems.forEach((el, index)=>{
+        let todo = document.createElement('div')
+        todo.className = "todo"
+
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.className ="checkbox"
+        checkbox.checked = el.status
+
+        let taskname = document.createElement('div')
+        taskname.textContent = el.taskname
+
+        todo.appendChild(checkbox)
+        todo.appendChild(taskname)
+
+        todosContainer.appendChild(todo)
+    })
+}
+
+displayTodos()
+
+
